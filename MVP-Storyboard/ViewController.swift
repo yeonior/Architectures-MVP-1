bdcaf2b97ab3e8e7d2266630256e523293f405d4
Rus <de.mr44@gmail.com>
2022-02-07 19:43:44 +0500

    Initial Commit

---
+++ MVP-Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Storyboard
+//
+//  Created by Ruslan on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
