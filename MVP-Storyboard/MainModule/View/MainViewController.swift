ce89152d582b6ac4e885d80899c35c91e0481474
Rus <de.mr44@gmail.com>
2022-02-07 20:49:51 +0500

    Added MainView and filled MainViewController a little bit

---
+++ MVP-Storyboard/MainModule/View/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  MVP-Storyboard
+//
+//  Created by Ruslan on 07.02.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    var presenter: MainPresenterProtocol!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - IBActions
+    @IBAction func buttonDidTapped(_ sender: UIButton) {
+        presenter.showGreeting()
+    }
+}
+
+extension MainViewController: MainViewProtocol {
+    
+    func setGreeting(greeting: String) {
+        greetingLabel.text = greeting
+    }
+}
+
